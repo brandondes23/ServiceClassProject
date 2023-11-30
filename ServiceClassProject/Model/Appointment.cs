2e0c5c2eb3ef8072c728cad9a907d554fbeca507
joefrancl <joefrancl2020@gmail.com>
2023-11-30 13:25:49 -0600

    create ...

---
+++ ServiceClassProject/Model/Appointment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceClassProject.Model;
+
+internal class Appointment
+{
+    public int Id { get; set; }
+
+    public DateTime date { get; set; }
+
+
+}
