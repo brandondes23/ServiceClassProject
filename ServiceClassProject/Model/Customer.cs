6662979f72af72e15b5c2e5db19563d561b6e756
joefrancl <joefrancl2020@gmail.com>
2023-11-30 13:12:53 -0600

    created customer class

---
+++ ServiceClassProject/Model/Customer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceClassProject.Model;
+
+internal class Customer
+{
+    public int ID { get; set; }
+
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+
+    public string Username { get; set; }
+
+
+}
